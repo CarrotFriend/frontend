f24792bc66335eb974a8ac6f3fea9982a590659a
seonghun <pch468@naver.com>
2022-02-27 22:40:26 +0900

    test: add Image.test.js

--- src/components/atoms/Image/Image.test.js
+++ src/components/atoms/Image/Image.test.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import { unmountComponentAtNode } from 'react-dom';
 import { render } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
-
 import Image from './index';
 
-// let container = null;
-// beforeEach(() => {
-//   container = document.createElement('div');
-//   document.body.appendChild(container);
-// });
-
-// afterEach(() => {
-//   unmountComponentAtNode(container);
-//   container.remove();
-//   container = null;
-// });
-
-describe('renders a image with right src or wrong src or without src', () => {
-  it('renders a image with right src', async () => {
-    const { getByAltText } = await render(<Image src="/item.jpg" alt="item" />);
+describe('renders a image', () => {
+  it('renders a image with right source', () => {
+    const { getByAltText } = render(<Image src="/item.jpg" alt="item" />);
     const image = getByAltText('item');
-
     expect(image.src).toContain('item.jpg');
+  });
+
+  it('renders a image without source', () => {
+    const { getByAltText } = render(<Image alt="item" />);
+    const image = getByAltText('item');
+    expect(image.src).toBe('');
+  });
+
+  it('renders a image without alt', () => {
+    const { getByRole } = render(<Image src="/item.jpg" />);
+    const image = getByRole('img');
+    expect(image.alt).toBe('');
   });
 });
